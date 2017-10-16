7b714107c7847cbdae897e6885481906c39fd4f2
Jan Hellar <jhellar@redhat.com>
2017-10-16 12:20:23 +0200

    Fix test require

--- parallel-runner.js
+++ parallel-runner.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 const parallel = require('./parallel-tests');
 
 const configFile = fs.readFileSync(process.argv[2], 'utf8');
 const config = JSON.parse(configFile);
 
-config.testFiles.forEach(testFile => require(testFile));
+config.testFiles.forEach(testFile => require(path.resolve(process.cwd(), testFile)));
 
 parallel.run();
